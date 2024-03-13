bbc7c34503adaff68641545f495d84f075200507
Andy <babyandy0111@gmail.com>
2024-03-14 00:54:48 +0800

    init

--- CropImageView/View/CustomImagePicker.swift
+++ CropImageView/View/CustomImagePicker.swift
@@ -4,7 +4,7 @@
 //
 //  Created by andy on 2024/3/13.
 //
-
+import UIKit
 import SwiftUI
 import PhotosUI
 
@@ -15,6 +15,12 @@ extension View {
         CustomImagePicker(options: options, show: show, croppedImage: croppedImage) {
             self
         }
+    }
+
+    // 只是個簡單裁減範例
+    @ViewBuilder
+    func frame(_ size: CGSize) -> some View {
+        self.frame(width: size.width, height: size.height)
     }
 }
 
@@ -33,8 +39,10 @@ fileprivate struct CustomImagePicker<Content: View>: View {
     }
 
     @State private var photosItem: PhotosPickerItem?
-    @State private var selectImage: UIImage?
+    @State private var selectedImage: UIImage?
     @State private var showDialog: Bool = false
+    @State private var selectedCropType: Crop = .circle
+    @State private var showCropView: Bool = false
 
     var body: some View {
         content
@@ -46,7 +54,7 @@ fileprivate struct CustomImagePicker<Content: View>: View {
                     if let imageData = try? await newValue.loadTransferable(type: Data.self), let image = UIImage(data: imageData) {
                         // 更新UI
                         await MainActor.run(body: {
-                            selectImage = image
+                            selectedImage = image
                             showDialog.toggle()
                         })
                     }
@@ -58,6 +66,179 @@ fileprivate struct CustomImagePicker<Content: View>: View {
             // 顯示所有的遮罩
             ForEach(options.indices, id: \.self) { index in
                 Button(options[index].name()) {
+                    selectedCropType = options[index]
+                    showCropView.toggle()
+                }
+            }
+        }
+            .fullScreenCover(isPresented: $showCropView) {
+            selectedImage = nil
+        } content: {
+            CropView(crop: selectedCropType, image: selectedImage) { croppedImage, status in
+
+            }
+        }
+    }
+}
+
+// 建立剪裁的View
+struct CropView: View {
+    var crop: Crop
+    var image: UIImage?
+    var onCrop: (UIImage?, Bool) -> ()
+
+    // 關閉當前頁面
+    @Environment(\.dismiss) private var dismiss
+
+    // 手勢屬性
+    @State private var scale: CGFloat = 1 // 放大預設倍數
+    @State private var lastScale: CGFloat = 0 // 最後放大預設倍數
+    @State private var offset: CGSize = .zero // 位置
+    @State private var lastStoredOffset: CGSize = .zero // 最後位置
+    @GestureState private var isInteracting: Bool = false // 互動
+
+    var body: some View {
+        NavigationStack {
+            ImageView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background { Color.black.ignoresSafeArea() }
+                .navigationTitle("crop view")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarBackground(.visible, for: .navigationBar)
+                .toolbarBackground(Color.black, for: .navigationBar)
+                .toolbarColorScheme(.dark, for: .navigationBar)
+                .toolbar {
+                ToolbarItem (placement: .navigationBarTrailing) {
+                    Button {
+
+                    } label: {
+                        Image(systemName: "checkmark")
+                            .font(.callout)
+                            .fontWeight(.semibold)
+                    }
+                }
+
+                ToolbarItem (placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.callout)
+                            .fontWeight(.semibold)
+                    }
+                }
+            }
+        }
+    }
+
+    // 顯示選取圖片
+    @ViewBuilder
+    func ImageView() -> some View {
+        let cropSize = crop.size()
+        GeometryReader {
+            let size = $0.size
+
+            if let image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .overlay(content: {
+                    GeometryReader { proxy in
+                        let rect = proxy.frame(in: .named("CROPVIEW"))
+
+                        Color.clear
+                            .onChange(of: isInteracting) { oldValue, newValue in
+
+                            // 這邊要進行拖動時判斷，超過就要拉回
+                            withAnimation(.easeInOut(duration: 0.2)) {
+                                // 先處理 mix x,y
+                                if rect.minX > 0 {
+                                    offset.width = (offset.width - rect.minX)
+                                }
+
+                                if rect.minY > 0 {
+                                    offset.height = (offset.height - rect.minY)
+                                }
+
+                                // 接續處理 max x,y
+                                if rect.maxX < size.width {
+                                    offset.width = (rect.minX - offset.width)
+                                }
+
+                                if rect.maxY < size.height {
+                                    offset.height = (rect.minY - offset.height)
+                                }
+                            }
+
+
+                            // true 拖動, false 停止拖動
+                            if !newValue {
+                                lastStoredOffset = offset
+                            }
+                        }
+                    }
+                }).frame(size)
+
+            }
+        }
+            .scaleEffect(scale) // 值來至於 MagnifyGesture onChanged中的變動
+        .offset(offset) // 值來至於 DragGesture onChanged中的變動
+        .overlay(content: Grids)
+            .coordinateSpace(name: "CROPVIEW")
+            .gesture(
+            DragGesture().updating($isInteracting, body: { _, out, _ in
+                out = true
+            }).onChanged({ value in
+                let translation = value.translation
+                offset = CGSize(width: translation.width + lastStoredOffset.width, height: translation.height + lastStoredOffset.height)
+
+            })
+        )
+            .gesture(
+            MagnificationGesture().updating($isInteracting, body: { _, out, _ in
+                out = true
+            }).onChanged({ value in
+                let updatedScale = value + lastScale
+                scale = (updatedScale < 1 ? 1 : updatedScale)
+            }).onEnded({ value in
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    if scale < 1 {
+                        scale = 1
+                        lastScale = 0
+                    } else {
+                        lastScale = scale - 1
+                    }
+                }
+            })
+        )
+            .frame(cropSize)
+            .cornerRadius(crop == .circle ? cropSize.height / 2 : 0)
+
+
+    }
+
+    // 來個比例線條吧
+    @ViewBuilder
+    func Grids() -> some View {
+        ZStack {
+            HStack {
+                ForEach(1...5, id: \.self) { _ in
+                    Rectangle()
+                        .fill(.white.opacity(0.7))
+                        .frame(width: 1)
+                        .frame(maxWidth: .infinity)
+
+                }
+            }
+        }
+
+        ZStack {
+            HStack {
+                ForEach(1...8, id: \.self) { _ in
+                    Rectangle()
+                        .fill(.white.opacity(0.7))
+                        .frame(height: 1)
+                        .frame(maxHeight: .infinity)
 
                 }
             }
@@ -67,6 +248,9 @@ fileprivate struct CustomImagePicker<Content: View>: View {
 
 struct CustomImagePicker_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        // 這邊可以切換crop type
+        CropView(crop: .square, image: UIImage(named: "1")) { _, _ in
+
+        }
     }
 }
