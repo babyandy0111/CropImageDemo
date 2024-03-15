66a8b22b8c382f4d98766290ec81573e9ab2f095
DHG-Andy <151802590+DHG-Andy@users.noreply.github.com>
2024-03-15 16:35:27 +0800

    add ffmpeg

---
+++ CropImageView/View/ImageHome.swift
@@ -0,0 +1,51 @@
+//
+//  Home.swift
+//  CropImageView
+//
+//  Created by andy on 2024/3/13.
+//
+
+import SwiftUI
+
+struct ImageHome: View {
+    @State private var showPicker: Bool = false
+    @State private var croppedImage: UIImage?
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                if let croppedImage {
+                    Image(uiImage: croppedImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300, height: 400)
+                } else {
+                    Text("No image select")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+
+                }
+            }
+                .navigationTitle("test crop")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                ToolbarItem (placement: .navigationBarTrailing) {
+                    Button {
+                        showPicker.toggle()
+                    } label: {
+                        Image(systemName: "photo.on.rectangle.angled")
+                            .font(.callout)
+                    }
+                        .tint(.black)
+                }
+            }
+                .cropImagePicker(options: [.circle, .square, .rectangle, .custom(.init(width: 300, height: 300))], show: $showPicker, croppedImage: $croppedImage)
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageHome()
+    }
+}
---
+++ CropImageView/View/VideoHome.swift
@@ -0,0 +1,18 @@
+//
+//  VideoHome.swift
+//  CropImageView
+//
+//  Created by andy.wang on 2024/3/15.
+//
+
+import SwiftUI
+
+struct VideoHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoHome()
+}
